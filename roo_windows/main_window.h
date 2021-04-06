5e6aa0ae4592d31c13434f3d979929da382e551b
Dawid Kurzyniec <dawidk@google.com>
2021-04-06 21:34:11 +0200

    Adding main window.

--- roo_windows/main_window.h
+++ roo_windows/main_window.h
@@ -1,16 +1,17 @@
 #pragma once
 
-#include <memory>
-
-#include "roo_display/core/drawable.h"
-#include "roo_windows/widget.h"
+#include "roo_display.h"
+#include "roo_windows/panel.h"
 
 namespace roo_windows {
 
-class WindowManager {
+class MainWindow : public Panel {
  public:
-  WindowManager(roo_display::Display* display, Widget* main)
-      : display_(display), main_(main), touch_down_(false) {}
+  MainWindow(roo_display::Display* display)
+      : MainWindow(display, display->extents()) {}
+
+  MainWindow(roo_display::Display* display, const Box& bounds)
+      : Panel(nullptr, bounds), display_(display), touch_down_(false) {}
 
   void tick() {
     // Check if no events to process.
@@ -18,18 +19,18 @@ class WindowManager {
     bool down = display_->getTouch(&x, &y);
     if (!touch_down_ && down) {
       // Pressed.
-      main_->onTouch(TouchEvent(TouchEvent::DOWN, x, y));
+      onTouch(TouchEvent(TouchEvent::DOWN, x, y));
     } else if (touch_down_ && !down) {
       // Released.
-      main_->onTouch(TouchEvent(TouchEvent::UP, touch_x_, touch_y_));
+      onTouch(TouchEvent(TouchEvent::UP, touch_x_, touch_y_));
     } else if (down) {
-      main_->onTouch(TouchEvent(TouchEvent::MOVE, x, y));
+      onTouch(TouchEvent(TouchEvent::MOVE, x, y));
     }
     touch_x_ = x;
     touch_y_ = y;
 
     roo_display::DrawingContext dc(display_);
-    dc.draw(Adapter(main_.get()));
+    dc.draw(Adapter(this));
   }
 
  private:
@@ -48,7 +49,6 @@ class WindowManager {
   };
 
   roo_display::Display* display_;
-  std::unique_ptr<Widget> main_;
   int16_t touch_x_, touch_y_;
   bool touch_down_;
 };
