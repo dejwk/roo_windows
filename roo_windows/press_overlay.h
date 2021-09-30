f0b5186d56c035c970d78d754d869820bd9abcee
Dawid Kurzyniec <dawidk@google.com>
2021-09-30 20:44:38 +0200

    Allowing PressOverlay to have a non-trivial background color. This allows to integrate any other possible overlay effect to the press overlay background.

--- roo_windows/press_overlay.h
+++ roo_windows/press_overlay.h
@@ -4,8 +4,9 @@
 
 class PressOverlay : public roo_display::Rasterizable {
  public:
-  PressOverlay(int16_t x, int16_t y, int16_t r, roo_display::Color color)
-      : x_(x), y_(y), r_(r), color_(color) {}
+  PressOverlay(int16_t x, int16_t y, int16_t r, roo_display::Color fg,
+               roo_display::Color bg = roo_display::color::Transparent)
+      : x_(x), y_(y), r_(r), fg_(fg), bg_(bg) {}
 
   virtual roo_display::Box extents() const override {
     return roo_display::Box(x_ - r_ - 1, y_ - r_ - 1, x_ + r_ + 1, y_ + r_ + 1);
@@ -13,18 +14,19 @@ class PressOverlay : public roo_display::Rasterizable {
 
   virtual void ReadColors(const int16_t* x, const int16_t* y, uint32_t count,
                           roo_display::Color* result) const {
-    int32_t r_sq = r_*r_;
+    int32_t r_sq = r_ * r_;
     while (count-- > 0) {
       int16_t dx = (*x++ - x_);
       int16_t dy = (*y++ - y_);
-      int32_t delta = r_sq - (dx*dx + dy*dy);
-      *result++ = (delta < 0 ? roo_display::color::Transparent : color_);
-    }                            
+      int32_t delta = r_sq - (dx * dx + dy * dy);
+      *result++ = (delta < 0 ? bg_ : fg_);
+    }
   }
 
  private:
   int16_t x_;
   int16_t y_;
   int16_t r_;
-  roo_display::Color color_;
+  roo_display::Color fg_;
+  roo_display::Color bg_;
 };
