0e4214912befb0ee12e51d19207b2b669c7d68d2
Dawid Kurzyniec <dawidk@google.com>
2021-10-20 22:42:49 +0200

    Fixing compilation errors in walltime.h, dependent on the order of header inclusion.

--- roo_windows/indicators/18/walltime.h
+++ roo_windows/indicators/18/walltime.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "roo_display.h"
+#include "roo_display/ui/text_label.h"
+#include "roo_display/ui/tile.h"
 #include "roo_smooth_fonts/NotoSans_Regular/18.h"
 #include "roo_windows/indicators/impl/walltime.h"
 
@@ -15,8 +18,10 @@ class WalltimeIndicator18 : public WalltimeIndicator {
       : WalltimeIndicator(parent, Box(dx, dy, dx + 45, dy + 17), clock, tz) {}
 
   void paint(const roo_display::Surface& s) override {
-    s.drawObject(MakeTileOf(TextLabel(font_NotoSans_Regular_18(), val_, color_),
-                            bounds(), HAlign::None(), VAlign::Middle()));
+    s.drawObject(roo_display::MakeTileOf(
+        roo_display::TextLabel(roo_display::font_NotoSans_Regular_18(), val_,
+                               color_),
+        bounds(), roo_display::HAlign::None(), roo_display::VAlign::Middle()));
   }
 };
 
