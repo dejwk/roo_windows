5b61afe9bf30bb9371e726fc7d43c1ac57f2c66b
Dawid Kurzyniec <dawidk@google.com>
2021-11-21 16:00:48 +0100

    Factoring toggle button implementation to use a cpp file, and fixing a possible memory leak.

---
+++ roo_windows/widgets/toggle_buttons.cpp
@@ -0,0 +1,81 @@
+#include "roo_windows/widgets/toggle_buttons.h"
+
+#include "roo_display/shape/basic_shapes.h"
+#include "roo_display/ui/tile.h"
+
+namespace roo_windows {
+
+roo_windows::Widget* ToggleButtons::addButton(const MonoIcon& icon) {
+  int16_t padding = 4;
+  int16_t width = 0;
+  int16_t height = 0;
+  for (const auto& i : buttons_) {
+    const Box& extents = i->icon().extents();
+    width += extents.width();
+    width += 2 * padding;
+    height = std::max(height, extents.height());
+  }
+  height = std::max(height, icon.extents().height());
+  height += 2 * padding;
+  moveTo(Box(0, 0, width + icon.extents().width() + 2 * padding + 1, height - 1)
+             .translate(xOffset(), yOffset()));
+  ToggleButton* btn = new ToggleButton(env_, icon);
+  Panel::add(btn,
+             Box(width + 1, 0, width + icon.extents().width() + 2 * padding,
+                 icon.extents().height() + 2 * padding - 1));
+
+  int idx = buttons_.size();
+  btn->setOnClicked([this, idx] { setActive(idx); });
+  buttons_.emplace_back(btn);
+  return btn;
+}
+
+bool ToggleButtons::paint(const roo_display::Surface& s) {
+  Color border = theme().color.defaultColor(s.bgcolor());
+  border.set_a(0x30);
+  s.drawObject(roo_display::Line(0, 0, 0, 0, roo_display::color::Transparent));
+  s.drawObject(roo_display::Line(0, 1, 0, height() - 2, border));
+  s.drawObject(roo_display::Line(0, height() - 1, 0, height() - 1,
+                                 roo_display::color::Transparent));
+  int16_t x = width() - 1;
+  s.drawObject(roo_display::Line(x, 0, x, 0, roo_display::color::Transparent));
+  s.drawObject(roo_display::Line(x, 1, x, height() - 2, border));
+  s.drawObject(roo_display::Line(x, height() - 1, x, height() - 1,
+                                 roo_display::color::Transparent));
+  return true;
+}
+
+void ToggleButtons::setActive(int index) {
+  if (index == active_) return;
+  active_ = index;
+  for (int i = 0; i < buttons_.size(); i++) {
+    buttons_[i]->setActivated(i == index);
+  }
+}
+
+bool ToggleButtons::ToggleButton::paint(const roo_display::Surface& s) {
+  Color color = theme().color.defaultColor(s.bgcolor());
+  Color internal_border = color;
+  internal_border.set_a(0x10);
+  Color external_border = color;
+  external_border.set_a(0x30);
+  s.drawObject(roo_display::Line(0, 0, width() - 1, 0, external_border));
+  s.drawObject(roo_display::Line(0, 1, 0, height() - 3, internal_border));
+  s.drawObject(roo_display::Line(1, 1, width() - 2, 1, internal_border));
+  MonoIcon icon(icon_);
+  icon.color_mode().setColor(color);
+  roo_display::Tile tile(&icon,
+                         Box(bounds().xMin() + 1, bounds().yMin() + 2,
+                             bounds().xMax() - 1, bounds().yMax() - 2),
+                         roo_display::HAlign::Center(),
+                         roo_display::VAlign::Middle(),
+                         roo_display::color::Transparent, s.fill_mode());
+  s.drawObject(tile);
+  s.drawObject(roo_display::Line(width() - 1, 0, width() - 1, height() - 3,
+                                 external_border));
+  s.drawObject(roo_display::FilledRect(0, height() - 2, width() - 1,
+                                       height() - 1, external_border));
+  return true;
+}
+
+}  // namespace roo_windows
--- roo_windows/widgets/toggle_buttons.h
+++ roo_windows/widgets/toggle_buttons.h
@@ -1,15 +1,9 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 
-#include "roo_display/filter/color_filter.h"
-#include "roo_display/ui/alignment.h"
-#include "roo_display/ui/text_label.h"
-#include "roo_material_icons.h"
-#include "roo_windows/core/main_window.h"
 #include "roo_windows/core/panel.h"
-#include "roo_windows/core/theme.h"
-#include "roo_windows/widgets/icon.h"
 
 namespace roo_windows {
 
@@ -17,58 +11,14 @@ class ToggleButtons : public Panel {
  public:
   ToggleButtons(const Environment& env) : Panel(env), env_(env), active_(-1) {}
 
-  roo_windows::Widget* addButton(const MonoIcon& icon) {
-    int16_t padding = 4;
-    int16_t width = 0;
-    int16_t height = 0;
-    for (const auto& i : buttons_) {
-      const Box& extents = i->icon().extents();
-      width += extents.width();
-      width += 2 * padding;
-      height = std::max(height, extents.height());
-    }
-    height = std::max(height, icon.extents().height());
-    height += 2 * padding;
-    moveTo(
-        Box(0, 0, width + icon.extents().width() + 2 * padding + 1, height - 1)
-            .translate(xOffset(), yOffset()));
-    ToggleButton* btn = new ToggleButton(env_, icon);
-    Panel::add(btn,
-               Box(width + 1, 0, width + icon.extents().width() + 2 * padding,
-                   icon.extents().height() + 2 * padding - 1));
+  roo_windows::Widget* addButton(const MonoIcon& icon);
 
-    int idx = buttons_.size();
-    btn->setOnClicked([this, idx] { setActive(idx); });
-    buttons_.push_back(btn);
-    return btn;
-  }
-
-  bool paint(const roo_display::Surface& s) override {
-    Color border = theme().color.defaultColor(s.bgcolor());
-    border.set_a(0x30);
-    s.drawObject(
-        roo_display::Line(0, 0, 0, 0, roo_display::color::Transparent));
-    s.drawObject(roo_display::Line(0, 1, 0, height() - 2, border));
-    s.drawObject(roo_display::Line(0, height() - 1, 0, height() - 1,
-                                   roo_display::color::Transparent));
-    int16_t x = width() - 1;
-    s.drawObject(
-        roo_display::Line(x, 0, x, 0, roo_display::color::Transparent));
-    s.drawObject(roo_display::Line(x, 1, x, height() - 2, border));
-    s.drawObject(roo_display::Line(x, height() - 1, x, height() - 1,
-                                   roo_display::color::Transparent));
-    return true;
-  }
+  // Draws left- and right-side framing around the togglebutton control.
+  bool paint(const roo_display::Surface& s) override;
 
   int getActive() const { return active_; }
 
-  void setActive(int index) {
-    if (index == active_) return;
-    active_ = index;
-    for (int i = 0; i < buttons_.size(); i++) {
-      buttons_[i]->setActivated(i == index);
-    }
-  }
+  void setActive(int index);
 
  private:
   class ToggleButton : public roo_windows::Widget {
@@ -79,30 +29,7 @@ class ToggleButtons : public Panel {
     bool useOverlayOnActivation() const override { return true; }
     bool isClickable() const override { return true; }
 
-    bool paint(const roo_display::Surface& s) override {
-      Color color = theme().color.defaultColor(s.bgcolor());
-      Color internal_border = color;
-      internal_border.set_a(0x10);
-      Color external_border = color;
-      external_border.set_a(0x30);
-      s.drawObject(roo_display::Line(0, 0, width() - 1, 0, external_border));
-      s.drawObject(roo_display::Line(0, 1, 0, height() - 3, internal_border));
-      s.drawObject(roo_display::Line(1, 1, width() - 2, 1, internal_border));
-      MonoIcon icon(icon_);
-      icon.color_mode().setColor(color);
-      roo_display::Tile tile(&icon,
-                             Box(bounds().xMin() + 1, bounds().yMin() + 2,
-                                 bounds().xMax() - 1, bounds().yMax() - 2),
-                             roo_display::HAlign::Center(),
-                             roo_display::VAlign::Middle(),
-                             roo_display::color::Transparent, s.fill_mode());
-      s.drawObject(tile);
-      s.drawObject(roo_display::Line(width() - 1, 0, width() - 1, height() - 3,
-                                     external_border));
-      s.drawObject(roo_display::FilledRect(0, height() - 2, width() - 1,
-                                           height() - 1, external_border));
-      return true;
-    }
+    bool paint(const roo_display::Surface& s) override;
 
     const MonoIcon& icon() const { return icon_; }
 
@@ -122,7 +49,7 @@ class ToggleButtons : public Panel {
   int active_;
 
   const Environment& env_;
-  std::vector<ToggleButton*> buttons_;
+  std::vector<std::unique_ptr<ToggleButton>> buttons_;
 };
 
 }  // namespace roo_windows
