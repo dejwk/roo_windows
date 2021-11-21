c4ba75be9c5d21cf9e1633e61b1edb5e11e00588
Dawid Kurzyniec <dawidk@google.com>
2021-11-21 20:57:33 +0100

    Revised the StaticLayout to add the ability to place items with their natural dimension, and aligned not necessarily at top-left, but also to the right and to the middle, both for the X and Y axes.

--- roo_windows/containers/static_layout.h
+++ roo_windows/containers/static_layout.h
@@ -22,6 +22,31 @@ class StaticLayout : public Panel {
   void add(Widget* child, const roo_display::Box& box) {
     Panel::add(child, box);
   }
+
+  /**
+   * Adds the specified child, at the end of the list, positioning it using its
+   * current natural dimensions, with its top left corner anchored at the
+   * specified point in the parent's coordinates.
+   */
+  void add(Widget* child, int16_t x, int16_t y) {
+    return add(child, x, y, roo_display::HAlign::Left(),
+               roo_display::VAlign::Top());
+  }
+
+  /**
+   * Adds the specified child, at the end of the list, positioning it using its
+   * current natural dimensions, anchored at the specified point in the parent's
+   * coordinates, with the specified alignment of the anchor relative to the
+   * contents.
+   */
+  void add(Widget* child, int16_t x, int16_t y, roo_display::HAlign halign,
+           roo_display::VAlign valign) {
+    Box anchor(x, y, x, y);
+    Dimensions d = child->getNaturalDimensions();
+    Box inner(0, 0, d.width() - 1, d.height() - 1);
+    add(child, inner.translate(halign.GetOffset(anchor, inner),
+                               valign.GetOffset(anchor, inner)));
+  }
 };
 
 }  // namespace roo_windows
