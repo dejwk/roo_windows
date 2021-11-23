b91e16a8c60a23c99e1a1ecb826565a3cfa9b144
Dawid Kurzyniec <dawidk@google.com>
2021-11-23 03:20:38 +0100

    Added a parameterless constructor to Dimensions.

--- roo_windows/core/dimensions.h
+++ roo_windows/core/dimensions.h
@@ -5,6 +5,7 @@ namespace roo_windows {
 // Width and height of a screen object.
 class Dimensions {
  public:
+  Dimensions() : Dimensions(0, 0) {}
   Dimensions(int16_t width, int16_t height) : width_(width), height_(height) {}
 
   int16_t width() const { return width_; }
