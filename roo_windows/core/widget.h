e3c1c9dc941b4eb6b92f68be565d04ea49ecacf5
Dawid Kurzyniec <dawidk@google.com>
2021-11-25 20:54:57 +0100

    Moving touch event to a separate file.

--- roo_windows/core/widget.h
+++ roo_windows/core/widget.h
@@ -16,6 +16,7 @@
 #include "roo_windows/core/measure_spec.h"
 #include "roo_windows/core/padding.h"
 #include "roo_windows/core/preferred_size.h"
+#include "roo_windows/core/touch_event.h"
 #include "roo_windows/core/theme.h"
 
 namespace roo_windows {
@@ -61,35 +62,6 @@ static const uint8_t kDirty = 0x01;
 static const uint8_t kInvalidated = 0x02;
 static const uint8_t kLayoutRequested = 0x04;
 static const uint8_t kLayoutRequired = 0x08;
-
-class TouchEvent {
- public:
-  enum Type { PRESSED, RELEASED, MOVED, DRAGGED, SWIPED };
-
-  TouchEvent(Type type, unsigned long duration_ms, int16_t x_start,
-             int16_t y_start, int16_t x_end, int16_t y_end)
-      : type_(type),
-        x_start_(x_start),
-        y_start_(y_start),
-        x_end_(x_end),
-        y_end_(y_end),
-        duration_ms_(duration_ms) {}
-
-  Type type() const { return type_; }
-  unsigned long duration() const { return duration_ms_; }
-  int16_t startX() const { return x_start_; }
-  int16_t startY() const { return y_start_; }
-  int16_t x() const { return x_end_; }
-  int16_t y() const { return y_end_; }
-
-  int16_t dx() const { return x() - startX(); }
-  int16_t dy() const { return y() - startY(); }
-
- private:
-  Type type_;
-  int16_t x_start_, y_start_, x_end_, y_end_;
-  unsigned long duration_ms_;
-};
 
 class Widget {
  public:
