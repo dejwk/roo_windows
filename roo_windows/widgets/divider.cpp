9ddb3e6c7b67868530948c8ab1c8314f13d7faab
Dawid Kurzyniec <dawidk@google.com>
2022-02-19 02:04:18 +0100

    Fix: drawing full area of the divider, if it gets forcibly rescaled.

--- roo_windows/widgets/divider.cpp
+++ roo_windows/widgets/divider.cpp
@@ -10,7 +10,7 @@ bool HorizontalDivider::paint(const Surface& s) {
   const Box box = bounds();
   Color color = theme().color.onBackground;
   color.set_a(0x40);
-  s.drawObject(roo_display::FilledRect(box.xMin(), box.yMax() - 1, box.xMax(),
+  s.drawObject(roo_display::FilledRect(box.xMin(), box.yMin(), box.xMax(),
                                        box.yMax() - 1, color));
   color.set_a(0x20);
   s.drawObject(roo_display::FilledRect(box.xMin(), box.yMax(), box.xMax(),
@@ -23,7 +23,7 @@ bool VerticalDivider::paint(const Surface& s) {
   const Box box = bounds();
   Color color = theme().color.onBackground;
   color.set_a(0x40);
-  s.drawObject(roo_display::FilledRect(box.xMax() - 1, box.yMin(),
+  s.drawObject(roo_display::FilledRect(box.xMin(), box.yMin(),
                                        box.xMax() - 1, box.yMax(), color));
   color.set_a(0x20);
   s.drawObject(roo_display::FilledRect(box.xMax(), box.yMin(), box.xMax(),
