45d709776e8674de624387826ca6633103bdceec
Dawid Kurzyniec <dawidk@google.com>
2022-06-05 11:57:20 +0200

    Adding the missing cpp file.

---
+++ roo_windows/core/activity.cpp
@@ -0,0 +1,12 @@
+#include "roo_windows/core/activity.h"
+#include "roo_windows/core/task.h"
+
+namespace roo_windows {
+
+roo_display::Box Activity::getPreferredPlacement(const Task& task) {
+  // By default, fill the entire task area.
+  Dimensions dims = task.getDimensions();
+  return roo_display::Box(0, 0, dims.width() - 1, dims.height() - 1);
+}
+
+}  // namespace roo_windows
