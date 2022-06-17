009fd2d9e8dca6568f7c4503899081001d749797
Dawid Kurzyniec <dawidk@google.com>
2022-06-17 20:11:00 +0200

    Added the 'progress bar' widget.

---
+++ roo_windows/widgets/progress_bar.cpp
@@ -0,0 +1,60 @@
+#include "roo_windows/widgets/progress_bar.h"
+
+#include <Arduino.h>
+
+#include "roo_display/shape/basic_shapes.h"
+
+using namespace roo_display;
+
+namespace roo_windows {
+
+bool ProgressBar::paint(const Surface& s) {
+  const Theme& th = theme();
+  Color c = (color_ == color::Transparent ? th.color.primary : color_);
+  if (progress_ >= 0) {
+    // Determinate.
+    int16_t xoffset_incomplete = (uint32_t)progress_ * width() / 10000;
+    if (xoffset_incomplete > 0) {
+      // There is some progress.
+      s.drawObject(roo_display::FilledRect(0, 0, xoffset_incomplete - 1,
+                                           height() - 1, c));
+    }
+    if (xoffset_incomplete < width()) {
+      // There is some left.
+      c.set_a(0x80);
+      s.drawObject(roo_display::FilledRect(xoffset_incomplete, 0, width() - 1,
+                                           height() - 1, c));
+    }
+    return true;
+  } else {
+    int16_t offset_start =
+        (uint32_t)(millis() % (1024 + 400) - 200) * width() / 1024;
+    int16_t offset_end = offset_start + width() / 5;
+    if (offset_start < 0) offset_start = 0;
+    if (offset_start >= width()) offset_start = width() - 1;
+    if (offset_end < 0) offset_end = 0;
+    if (offset_end >= width()) offset_end = width() - 1;
+    if (offset_start > 0) {
+      c.set_a(0x80);
+      s.drawObject(
+          roo_display::FilledRect(0, 0, offset_start - 1, height() - 1, c));
+    }
+    if (offset_start < width() - 1) {
+      c.set_a(0xFF);
+      s.drawObject(roo_display::FilledRect(offset_start, 0, offset_end,
+                                           height() - 1, c));
+    }
+    if (offset_end + 1 < width()) {
+      c.set_a(0x80);
+      s.drawObject(roo_display::FilledRect(offset_end + 1, 0, width() - 1,
+                                           height() - 1, c));
+    }
+    return false;
+  }
+}
+
+Dimensions ProgressBar::getSuggestedMinimumDimensions() const {
+  return Dimensions(20, 4);
+}
+
+}  // namespace roo_windows
---
+++ roo_windows/widgets/progress_bar.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "roo_windows/core/widget.h"
+
+namespace roo_windows {
+
+class ProgressBar : public Widget {
+ public:
+  ProgressBar(const Environment& env) : Widget(env), progress_(-1), color_(0) {}
+
+  bool paint(const Surface& s) override;
+
+  Dimensions getSuggestedMinimumDimensions() const override;
+
+  PreferredSize getPreferredSize() const override {
+    return PreferredSize(PreferredSize::MatchParent(), PreferredSize::Exact(4));
+  }
+
+  Margins getDefaultMargins() const override { return Margins(0); }
+  Padding getDefaultPadding() const override { return Padding(0); }
+
+  void setColor(roo_display::Color color) { color_ = color; }
+
+  void setIndeterminate() { setProgress(-1); }
+
+  void setProgress(int16_t progress) {
+    if (progress > 10000) progress = 10000;
+    if (progress < -1) progress = -1;
+    if (progress_ == progress) return;
+    progress_ = progress;
+    markDirty();
+  }
+
+  bool isIndeterminate() const { return (progress_ < 0); }
+
+ private:
+  // negative = indeterminate. Otherwise, in [0-10000], in 1/100 of a percent.
+  int16_t progress_;
+
+  roo_display::Color color_;
+};
+
+}  // namespace roo_windows
