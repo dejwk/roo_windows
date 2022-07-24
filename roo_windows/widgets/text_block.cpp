eaa365e6be4e881b7d970a7f19ea3a07c5d2a0c8
Dawid Kurzyniec <dawidk@google.com>
2022-07-24 21:02:50 +0200

    Added a text block.

---
+++ roo_windows/widgets/text_block.cpp
@@ -0,0 +1,113 @@
+#include "roo_windows/widgets/text_block.h"
+
+namespace roo_windows {
+
+namespace {
+
+class Interior : public roo_display::Drawable {
+ public:
+  Interior(Box extents, const std::string* value, const roo_display::Font& font,
+           roo_display::Color color)
+      : extents_(extents), value_(value), font_(font), color_(color) {}
+
+  Box extents() const override { return extents_; }
+
+ private:
+  void drawTo(const roo_display::Surface& s) const override {
+    if (value_->empty()) return;
+    int16_t line_height = font_.metrics().maxHeight();
+    int16_t y = 0;
+    const uint8_t* p = (const uint8_t*)&*value_->begin();
+    const uint8_t* end = (const uint8_t*)&*value_->end();
+    do {
+      const uint8_t* start = p;
+      while (p != end && *p != '\n') {
+        ++p;
+      }
+      Surface news = s;
+      news.set_dy(s.dy() + y + font_.metrics().glyphYMax());
+      auto metrics = font_.getHorizontalStringMetrics(start, p - start);
+      font_.drawHorizontalString(news, start, p - start, color_);
+      if (metrics.width() < extents_.width()) {
+        news.drawObject(roo_display::FilledRect(
+            metrics.width(), -font_.metrics().glyphYMax(), extents_.width() - 1,
+            -font_.metrics().glyphYMin(), news.bgcolor()));
+      }
+
+      y += line_height;
+
+      // Skip over the '\n' character.
+      if (p != end) ++p;
+    } while (p != end);
+  }
+
+  roo_display::Box extents_;
+  const std::string* value_;
+  const roo_display::Font& font_;
+  roo_display::Color color_;
+};
+
+}  // namespace
+
+TextBlock::TextBlock(const Environment& env, std::string value,
+                     const roo_display::Font& font, roo_display::Color color,
+                     roo_display::HAlign halign, roo_display::VAlign valign)
+    : Widget(env),
+      value_(),
+      font_(font),
+      color_(color),
+      halign_(halign),
+      valign_(valign) {
+  setContent(value);
+}
+
+bool TextBlock::paint(const roo_display::Surface& s) {
+  roo_display::Color color =
+      color_.a() == 0 ? parent()->defaultColor() : color_;
+  s.drawObject(roo_display::MakeTileOf(
+      Interior(Box(0, 0, text_dims_.width() - 1, text_dims_.height() - 1),
+               &value_, font_, color),
+      bounds(), halign_, valign_));
+  return true;
+}
+
+Dimensions TextBlock::getSuggestedMinimumDimensions() const {
+  return text_dims_;
+}
+
+void TextBlock::setContent(std::string value) {
+  if (value_ == value) return;
+  value_ = std::move(value);
+
+  if (value_.empty()) {
+    text_dims_ = Dimensions(0, 0);
+  } else {
+    int16_t line_count = 0;
+    int16_t max_line_width = 0;
+    const uint8_t* p = (const uint8_t*)&*value_.begin();
+    const uint8_t* end = (const uint8_t*)&*value_.end();
+    do {
+      ++line_count;
+      const uint8_t* start = p;
+      while (p != end && *p != '\n') {
+        ++p;
+      }
+      if (p != start) {
+        auto metrics = font_.getHorizontalStringMetrics(start, p - start);
+        if (metrics.width() > max_line_width) {
+          max_line_width = metrics.width();
+        }
+      }
+      // Skip over the '\n' character.
+      if (p != end) ++p;
+    } while (p != end);
+
+    text_dims_ =
+        Dimensions(max_line_width, line_count * font_.metrics().maxHeight());
+  }
+
+  markDirty();
+  requestLayout();
+}
+
+}  // namespace roo_windows
---
+++ roo_windows/widgets/text_block.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "roo_display/core/color.h"
+#include "roo_display/font/font.h"
+#include "roo_display/ui/text_label.h"
+#include "roo_display/ui/tile.h"
+#include "roo_windows/core/panel.h"
+
+namespace roo_windows {
+
+// Multi-line block of text. Use the '\n' characters to split the lines.
+class TextBlock : public Widget {
+ public:
+  TextBlock(const Environment& env, std::string value,
+            const roo_display::Font& font, roo_display::HAlign halign,
+            roo_display::VAlign valign)
+      : TextBlock(env, value, font, roo_display::color::Transparent, halign,
+                  valign) {}
+
+  TextBlock(const Environment& env, std::string value,
+            const roo_display::Font& font, roo_display::Color color,
+            roo_display::HAlign halign, roo_display::VAlign valign);
+
+  bool paint(const roo_display::Surface& s) override;
+
+  Dimensions getSuggestedMinimumDimensions() const override;
+
+  const std::string& content() const { return value_; }
+
+  void setContent(std::string value);
+
+  const roo_display::Font& font() const { return font_; }
+
+  Padding getDefaultPadding() const override { return Padding(0); }
+  Margins getDefaultMargins() const override { return Margins(0); }
+
+ private:
+  std::string value_;
+  Dimensions text_dims_;
+  const roo_display::Font& font_;
+  roo_display::Color color_;
+  roo_display::HAlign halign_;
+  roo_display::VAlign valign_;
+};
+
+}  // namespace roo_windows
