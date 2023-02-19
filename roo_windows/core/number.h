206258e78cd505ec2f8286a1997adb286ff36b1f
Dawid Kurzyniec <dawidk@google.com>
2023-02-19 23:40:08 +0100

    Small number: formatting and small tweaks to utility methods.

--- roo_windows/core/number.h
+++ roo_windows/core/number.h
@@ -9,14 +9,22 @@ namespace roo_windows {
 class SmallNumber {
  public:
   SmallNumber(uint16_t val) : val_(val << 4) {}
+
   SmallNumber(int val)
       : val_(val >= 4096 ? 65535
              : val < 0   ? 0
                          : ((uint16_t)val) << 4) {}
+
   SmallNumber(unsigned int val)
       : val_(val >= 4096 ? 65535 : ((uint16_t)val) << 4) {}
-  SmallNumber(float val) : val_(std::min(std::max(val * 16.0f, 0.0f), 65535.0f)) {}
-  SmallNumber(double val) : val_(std::min(std::max(val * 16.0, 0.0), 65535.0)) {}
+
+  SmallNumber(float val)
+      : val_(std::min(std::max(val * 16.0f, 0.0f), 65535.0f)) {}
+
+  SmallNumber(double val)
+      : val_(std::min(std::max(val * 16.0, 0.0), 65535.0)) {}
+
+  static SmallNumber Of16ths(uint16_t val) { return SmallNumber(val, true); }
 
   operator float() const { return ((float)val_) / 16; }
 
@@ -24,7 +32,7 @@ class SmallNumber {
   uint16_t floor() const { return val_ >> 4; }
 
   // Returns the integer ceiling of the number.
-  uint16_t ceil() const { return ((uint32_t)val_ + 15) >> 4;}// return 4096 - ((65536 - val_) >> 4); }
+  uint16_t ceil() const { return 4096 - ((65536 - val_) >> 4); }
 
   // Returns 16 * the fractional part of the number.
   uint8_t frac_16ths() const { return val_ & 15; }
