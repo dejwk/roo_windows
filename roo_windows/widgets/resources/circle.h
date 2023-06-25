45712b66be272209d94606fcf0548b88677c2461
Dawid Kurzyniec <dawidk@google.com>
2023-06-25 17:34:46 +0200

    Factoring common resources out of switch, to use them with a slider.

---
+++ roo_windows/widgets/resources/circle.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "roo_display/image/image.h"
+
+namespace roo_windows {
+
+const ::roo_display::Pictogram& circle_18();
+const ::roo_display::Pictogram& circle_24();
+const ::roo_display::Pictogram& circle_36();
+const ::roo_display::Pictogram& circle_48();
+
+}  // namespace roo_windows
---
+++ roo_windows/widgets/resources/circular_shadow.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "roo_display/image/image.h"
+
+namespace roo_windows {
+
+const ::roo_display::Pictogram& circular_shadow_18();
+const ::roo_display::Pictogram& circular_shadow_24();
+const ::roo_display::Pictogram& circular_shadow_36();
+const ::roo_display::Pictogram& circular_shadow_48();
+
+}  // namespace roo_windows
