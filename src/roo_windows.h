fd74b0936069c9ccb4a36c29b535adea172f77a7
dejwk <dawid.kurzyniec@gmail.com>
2023-12-04 23:15:50 +0100

    Adding a convenience public header that contains the core includes.

---
+++ src/roo_windows.h
@@ -0,0 +1,5 @@
+#include "roo_windows/config.h"
+#include "roo_windows/core/activity.h"
+#include "roo_windows/core/application.h"
+#include "roo_windows/core/environment.h"
+#include "roo_windows/core/task.h"
