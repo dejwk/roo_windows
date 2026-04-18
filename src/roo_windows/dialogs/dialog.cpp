99018b84c32bc704280ed6a330d14cdccb13791f
dejwk <dawid.kurzyniec@gmail.com>
2026-04-18 17:07:18 +0200

    Moved scrim definition to roo_display.

--- src/roo_windows/dialogs/dialog.cpp
+++ src/roo_windows/dialogs/dialog.cpp
@@ -6,32 +6,8 @@ namespace roo_windows {
 
 namespace {
 
-class Scrim : public roo_display::Rasterizable {
- public:
-  void readColors(const int16_t* x, const int16_t* y, uint32_t count,
-                  Color* result) const override {
-    FillColor(result, count, Color(0x80000000));
-  }
-
-  bool readColorRect(int16_t xMin, int16_t yMin, int16_t xMax, int16_t yMax,
-                     Color* result) const override {
-    *result = Color(0x80000000);
-    return true;
-  }
-
-  bool readUniformColorRect(int16_t xMin, int16_t yMin, int16_t xMax,
-                            int16_t yMax, Color* result) const override {
-    *result = Color(0x80000000);
-    return true;
-  }
-
-  roo_display::Box extents() const override {
-    return roo_display::Box::MaximumBox();
-  }
-};
-
-const Scrim& scrim() {
-  static Scrim scrim;
+const roo_display::Rasterizable& scrim() {
+  static roo_display::Scrim scrim;
   return scrim;
 }
 
