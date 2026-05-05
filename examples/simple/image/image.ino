e97d28fcaff9df85718e94f99d6eb533846eeca9
dejwk <dawid.kurzyniec@gmail.com>
2026-05-05 22:57:58 +0200

    Refactored the 'label' and 'image' examples.

--- examples/simple/image/image.ino
+++ examples/simple/image/image.ino
@@ -1,22 +1,57 @@
+
+// *************** EMULATOR SETUP BEGIN
+
+#ifdef ROO_TESTING
+
+#include "roo_testing/devices/display/ili9341/ili9341spi.h"
+#include "roo_testing/microcontrollers/esp32/fake_esp32.h"
+#include "roo_testing/transducers/ui/viewport/flex_viewport.h"
+#include "roo_testing/transducers/ui/viewport/fltk/fltk_viewport.h"
+
+using roo_testing_transducers::FlexViewport;
+using roo_testing_transducers::FltkViewport;
+
+struct Emulator {
+  FltkViewport viewport;
+  FlexViewport flexViewport;
+
+  FakeIli9341Spi display;
+
+  Emulator()
+      : viewport(),
+        flexViewport(viewport, 1, FlexViewport::kRotationRight),
+        display(flexViewport) {
+    FakeEsp32().attachSpiDevice(display, 4, 5, 6);
+    FakeEsp32().gpio.attachOutput(7, display.cs());
+    FakeEsp32().gpio.attachOutput(2, display.dc());
+    FakeEsp32().gpio.attachOutput(3, display.rst());
+  }
+} emulator;
+
+#endif
+
+// *************** DISPLAY SETUP BEGIN
+
 #include "Arduino.h"
 #include "roo_display.h"
-#include "roo_scheduler.h"
-#include "roo_windows.h"
 
 using namespace roo_display;
-using namespace roo_windows;
 
 // Select the driver to match your display device.
 #include "roo_display/driver/ili9341.h"
 #include "roo_display/driver/touch_xpt2046.h"
 
 // Set your configuration for the driver.
-static constexpr int kCsPin = 5;
-static constexpr int kDcPin = 17;
-static constexpr int kRstPin = 27;
-static constexpr int kBlPin = 16;
+static constexpr int kCsPin = 7;
+static constexpr int kDcPin = 2;
+static constexpr int kRstPin = 3;
+static constexpr int kBlPin = 20;
 
-static constexpr int kTouchCsPin = 2;
+static constexpr int kSpiSckPin = 4;
+static constexpr int kSpiMisoPin = 5;
+static constexpr int kSpiMosiPin = 6;
+
+static constexpr int kTouchCsPin = 1;
 
 // Uncomment if you have connected the BL pin to GPIO.
 
@@ -30,8 +65,18 @@ Display display(screen, touch,
                 TouchCalibration(269, 249, 3829, 3684,
                                  Orientation::LeftDown()));
 
-#include "roo_display/shape/smooth.h"
+void initDisplay() {
+  SPI.begin(kSpiSckPin, kSpiMisoPin, kSpiMosiPin);
+  display.init();
+}
+
+// *************** EXAMPLE STARTS HERE
+
+#include "roo_scheduler.h"
+#include "roo_windows.h"
 #include "roo_windows/widgets/image.h"
+
+using namespace roo_windows;
 
 roo_scheduler::Scheduler scheduler;
 Environment env(scheduler);
@@ -48,8 +93,7 @@ Image img(env, pie);
 SingletonActivity activity(app, img);
 
 void setup() {
-  SPI.begin();
-  display.init();
+  initDisplay();
   app.start();
 
   // Never exits.
