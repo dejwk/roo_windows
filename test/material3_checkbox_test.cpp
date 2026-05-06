d2eb01fad14ea680679a260109c5b2f6e2562245
dejwk <dawid.kurzyniec@gmail.com>
2026-05-07 00:46:31 +0200

    Strenghtening the checkbox test.

--- test/material3_checkbox_test.cpp
+++ test/material3_checkbox_test.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "roo_scheduler.h"
 #include "roo_windows/core/environment.h"
+#include "roo_windows/core/measure_spec.h"
 #include "roo_windows/material3/checkbox/checkbox.h"
 
 namespace roo_windows {
@@ -30,6 +31,36 @@ TEST(Material3Checkbox, ReportsMaterial3MinimumSize) {
 
   EXPECT_EQ(Scaled(18), dims.width());
   EXPECT_EQ(Scaled(18), dims.height());
+}
+
+TEST(Material3Checkbox, ReportsNaturalMeasureAsEighteenByEighteen) {
+  roo_scheduler::Scheduler scheduler;
+  Environment env(scheduler);
+
+  Checkbox checkbox(env, OnOffState::kOff);
+
+  Dimensions measured =
+      checkbox.measure(WidthSpec::Unspecified(0), HeightSpec::Unspecified(0));
+
+  EXPECT_EQ(Scaled(18), measured.width());
+  EXPECT_EQ(Scaled(18), measured.height());
+}
+
+TEST(Material3Checkbox, UsesZeroInsetsAndMeasuredContentBounds) {
+  roo_scheduler::Scheduler scheduler;
+  Environment env(scheduler);
+
+  Checkbox checkbox(env, OnOffState::kOff);
+  Dimensions measured =
+      checkbox.measure(WidthSpec::Unspecified(0), HeightSpec::Unspecified(0));
+  checkbox.layout(Rect(0, 0, measured.width() - 1, measured.height() - 1));
+
+  EXPECT_EQ(Padding(0), checkbox.getPadding());
+  EXPECT_EQ(Margins(0), checkbox.getMargins());
+  EXPECT_EQ(Rect(0, 0, measured.width() - 1, measured.height() - 1),
+            checkbox.bounds());
+  EXPECT_EQ(Rect(0, 0, measured.width() - 1, measured.height() - 1),
+            checkbox.getContentBounds());
 }
 
 TEST(Material3Checkbox, EffectiveContainerRoleTracksSelectionState) {
